9f716f844bc5e5f1b3d7b8d59d53360b3599b920
mideep1990 <mideepankar@gmail.com>
2023-04-27 17:41:10 +0530

    inn

--- SomeJS/main.js
+++ SomeJS/main.js
@@ -1,8 +1,35 @@
+/*//toReversed
 const arr = [1, 3, 15, 11, 4];
+console.log("Original : ",arr)
+const Revarr = arr.toReversed() 
+console.log("reversed : ",Revarr)
+console.log("Original : ",arr)*/
 
 
-console.log(arr.find((item) => item > 10));
-console.log(arr.findIndex((item) => item > 10));
 
-console.log(arr.findLast((item) => item > 10));
-console.log(arr.findLastIndex((item) => item > 10));
+
+/*//toSorted
+const arr = [1, 8, 5, 3, 4];
+console.log("Original : ",arr)
+const Sortarr = arr.toSorted() 
+console.log("Sorted : ",Sortarr)
+console.log("Original : ",arr)*/
+
+
+
+
+
+/*//toSpliced
+const arr = [1, 3, 55, 8, 4];
+console.log("Original : ",arr)
+arr.splice(1,0,99) 
+console.log("Original : ",arr)*/
+
+
+
+//toSpliced
+const arr = [1, 3, 55, 8, 4];
+console.log("Original : ",arr)
+const spliceArr = arr.toSpliced(1,0,99)
+console.log("Spliced Arr : ",spliceArr) 
+console.log("Original : ",arr)

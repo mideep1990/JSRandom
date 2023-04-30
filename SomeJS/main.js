4dac5c707b90f6922d22606efeb384d9b1349ea5
mideep1990 <mideepankar@gmail.com>
2023-04-30 20:16:48 +0530

    add

--- SomeJS/main.js
+++ SomeJS/main.js
@@ -1,35 +1,19 @@
-/*//toReversed
-const arr = [1, 3, 15, 11, 4];
-console.log("Original : ",arr)
-const Revarr = arr.toReversed() 
-console.log("reversed : ",Revarr)
-console.log("Original : ",arr)*/
+/*const address = {
+    name:"Deepankar", 
+    city:"Bng", 
+    pin:500121}; 
+
+for (let x in address) {
+  console.log(address[x])
+}
+
+*/
 
 
 
 
-/*//toSorted
-const arr = [1, 8, 5, 3, 4];
-console.log("Original : ",arr)
-const Sortarr = arr.toSorted() 
-console.log("Sorted : ",Sortarr)
-console.log("Original : ",arr)*/
+const iplTeams = ['RCB','CSK','MI'] 
 
-
-
-
-
-/*//toSpliced
-const arr = [1, 3, 55, 8, 4];
-console.log("Original : ",arr)
-arr.splice(1,0,99) 
-console.log("Original : ",arr)*/
-
-
-
-//toSpliced
-const arr = [1, 3, 55, 8, 4];
-console.log("Original : ",arr)
-const spliceArr = arr.toSpliced(1,0,99)
-console.log("Spliced Arr : ",spliceArr) 
-console.log("Original : ",arr)
+for (let t of iplTeams) {
+  console.log(t)
+}

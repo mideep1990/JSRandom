2ba0af942f2433ce4e6fd2dddbf01ce4654d9e6e
mideep1990 <mideepankar@gmail.com>
2023-05-25 23:23:49 +0530

    font

--- SomeJS/main.js
+++ SomeJS/main.js
@@ -1,8 +1,1 @@
-console.log("width : ",screen.width)
-console.log("height : ",screen.height)
-console.log("availwidth : ",screen.availWidth)
-console.log("availheight : ",screen.availHeight)
-console.log("orientation : ",screen.orientation)
-console.log("pixelDepth : ",screen.pixelDepth)
-console.log("colorDepth : ",screen.colorDepth)
 
